f2aecaafa531f85c094c88335d9213ec9b5e1a2a
varunTarone <var17t@gmail.com>
2025-01-30 15:14:51 +0530

    feat: header section done

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,36 @@
-export const Navbar = () => {
-  return null;
+import ArrowRight from "@/assets/arrow-right.svg";
+import Logo from "@/assets/logosaas.png";
+import Image from "next/image";
+import MenuIcon from "@/assets/menu.svg";
+
+export const Header = () => {
+  return( 
+    <header className="sticky top-0">
+      <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3"> 
+        <p className="text-white/60 hidden md:block">
+          Streamline your workflow and boost your productivity
+        </p>
+        <div className="inline-flex gap-1 items-center">
+          <p>Get started for free</p>
+          <ArrowRight className="w-4 h-4 inline-flex justify-center items-center"/>
+        </div>
+      </div>
+      <div className="py-5 ">
+        <div className="container">
+          <div className="flex justify-between items-center"> 
+            <Image src={Logo} alt="saas logo" width={40} height={40} />
+            <MenuIcon className="w-5 h-5 md:hidden"/>
+            <nav className="hidden md:flex gap-6 text-black/60 items-center">
+              <a href="#">About</a>
+              <a href="#">Features</a>
+              <a href="#">Customers</a>
+              <a href="#">Updates</a>
+              <a href="#">Help</a>
+              <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex align-items tracking-tight">Get for free</button>
+            </nav>
+          </div>  
+        </div>
+      </div>
+  </header>  
+  );
 };
