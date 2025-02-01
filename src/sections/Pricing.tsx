cdf049d27b36093c47ca703ae68fd6d8bfe80678
Varun <var17t@gmail.com>
2025-02-01 18:00:07 +0530

    feat: added more designs and completed pricing section

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,3 +1,7 @@
+import CheckIcon from "@/assets/check.svg"
+import { text } from "stream/consumers";
+import { twMerge } from "tailwind-merge";
+
 const pricingTiers = [
   {
     title: "Free",
@@ -51,5 +55,44 @@ const pricingTiers = [
 ];
 
 export const Pricing = () => {
-  return null;
+  return (
+    <section className="py-24">
+      <div className="container">
+        <div className="section-heading">
+          <h2 className="section-title">Pricing</h2>
+          <p className="section-description mt-5">Effortlessly turn your ideas into a fully functional, responsive, no-code SaaS website in just minutes with the set of free components for Framer.</p>
+        </div>
+        <div className="flex flex-col gap-6 items-center mt-10 lg:flex-row lg:items-end lg:justify-center">
+          {pricingTiers.map(({ title, monthlyPrice, buttonText, popular, inverse, features }) => (
+            <div className={twMerge("p-10 rounded-3xl shadow-[0_7px_14px_#EAEAEA] border-[#F1F1F1] max-w-xs w-full", inverse === true && 'border-black bg-black text-white')}>
+              <div className="flex justify-between">
+                <h3 className={twMerge("text-lg font-bold text-black/50", inverse === true && "text-white/60")}>{title}</h3>
+                {popular === true && (
+                  <div className="inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20">
+                    <span className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] text-transparent font-medium bg-clip-text">Popular</span>
+                  </div>
+                )}
+              </div> 
+              <div className="flex items-baseline gap-1 mt-[30px]">
+                <span className="text-4xl font-bold tracking-tighter leading-none">${monthlyPrice}</span>
+                <span className="tracking-tight font-bold text-black/50">/month</span>
+              </div>
+              <button className={twMerge("btn btn-primary w-full mt-[30px]", inverse === true && "bg-white text-black")}>{buttonText}</button>
+              <ul className="flex flex-col gap-5 mt-8">
+                {features.map((feature) => (
+                  <li className="text-sm flex items-center gap-4">
+                    <CheckIcon className="w-5 h-5" />
+                    <span>{feature}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+          <div>
+            <h3></h3>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,25 @@
+import productImage from "@/assets/product-image.png"
+import pyramidImage from "@/assets/pyramid.png"
+import tubeImage from "@/assets/tube.png"
+import Image from "next/image";
+
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
+      <div className="container">
+        <div className="section-heading">
+          <div className="flex justify-center">  
+            <div className="tag">Boost your productivity</div>
+          </div>
+          <h2 className="section-title mt-5">A more effective way to track Progress</h2>
+          <p className="section-description mt-5">Effortlessly turn your ideas into a fully functional, responsive, no-code SaaS website in just minutes with the set of free components for Framer.</p>
+        </div>
+      </div>
+      <div className="relative">
+        <Image src={productImage} alt="Product Image" className="mt-10" />
+        <Image src={pyramidImage} alt="Pyramid Image" height={262} width={262} className="hidden md:block absolute -right-36 -top-32" />
+        <Image src={tubeImage} alt="Tube Image" height={248} className="hidden md:block absolute -left-36 bottom-24" />
+      </div>
+    </section>
+  );
 };
