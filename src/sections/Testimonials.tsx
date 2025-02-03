b6b8ef43cacac050ddfa9afb2a299d9060090a30
Varun <var17t@gmail.com>
2025-02-03 16:20:42 +0530

    feat: animated testimonial section

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,3 +1,4 @@
+"use client";
 import avatar1 from "@/assets/avatar-1.png";
 import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
@@ -7,7 +8,10 @@ import avatar6 from "@/assets/avatar-6.png";
 import avatar7 from "@/assets/avatar-7.png";
 import avatar8 from "@/assets/avatar-8.png";
 import avatar9 from "@/assets/avatar-9.png";
+import { motion } from "framer-motion";
+import { div } from "framer-motion/client";
 import Image from "next/image";
+import React from "react";
 import { twMerge } from "tailwind-merge";
 
 const testimonials = [
@@ -71,21 +75,41 @@ const firstColumn = testimonials.slice(0, 3);
 const secondColumn = testimonials.slice(3, 6);
 const thirdColumn = testimonials.slice(6, 9);
 
-const TestimonialsColumn = ( props: { className?:string; testimonials: typeof testimonials}) => (
-  <div className={twMerge("flex flex-col gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)]", props.className)}> 
-      {props.testimonials.map(({text, imageSrc, name, username}) => (
-      <div className="card">
-          <div>{text}</div>
-          <div className="flex items-center gap-2 mt-5">
-            <Image src={imageSrc} width={40} height={40} alt={name} className="h-10 w-10 rounded-full"/>
-            <div className="flex flex-col">
-              <div className="font-medium tracking-tight leading-5">{name}</div>
-              <div className="leading-5 tracking-tight">{username}</div>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
+const TestimonialsColumn = ( props: { 
+  className?:string; 
+  testimonials: typeof testimonials;
+  duration?:number;
+}) => (
+  <div className={props.className}>
+    <motion.div 
+      animate={{
+        translateY: "-50%",
+      }} 
+      transition={{
+        repeat: Infinity,
+        repeatType: "loop",
+        duration: props.duration || 10,
+        ease: "linear",
+      }}
+    className="flex flex-col gap-6 pb-6"> 
+        {[...new Array(2)].fill(0).map((_, index) => (
+          <React.Fragment key={index}> 
+              {props.testimonials.map(({text, imageSrc, name, username}) => (
+                <div className="card">
+                    <div>{text}</div>
+                    <div className="flex items-center gap-2 mt-5">
+                      <Image src={imageSrc} width={40} height={40} alt={name} className="h-10 w-10 rounded-full"/>
+                      <div className="flex flex-col">
+                        <div className="font-medium tracking-tight leading-5">{name}</div>
+                        <div className="leading-5 tracking-tight">{username}</div>
+                      </div>
+                    </div>
+                  </div>
+              ))}
+          </React.Fragment>
+        ))}  
+      </motion.div>
+  </div>
 )
 
 export const Testimonials = () => {
@@ -101,10 +125,10 @@ export const Testimonials = () => {
             From intutive design to powerful features, you'll find everything you need to create extraordinary projects.
           </p>
         </div>
-        <div className="flex justify-center gap-6">
-          <TestimonialsColumn testimonials={firstColumn}/>
-          <TestimonialsColumn testimonials={secondColumn} className="hidden md:flex"/>
-          <TestimonialsColumn testimonials={thirdColumn} className="hidden lg:flex"/>
+        <div className="flex justify-center gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)] max-h-[738px] overflow-hidden">
+          <TestimonialsColumn testimonials={firstColumn} duration={15}/>
+          <TestimonialsColumn testimonials={secondColumn} className="hidden md:block" duration={19}/>
+          <TestimonialsColumn testimonials={thirdColumn} className="hidden lg:block" duration={17}/>
         </div>
       </div>
     </section>
